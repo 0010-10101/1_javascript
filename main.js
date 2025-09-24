96daa707beb76ceb309126fd9e5c389df0f0b384
Sahil Thool <sahilthool12@gmail.com>
2025-09-24 14:26:16 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let firstname = "John Doe";
+let age = 23;
+let isMarried = false;
+if(isMarried == true){
+    console.log(firstname + " is Married.");
+}
+else{
+    console.log(firstname + " is not married.");
+}
